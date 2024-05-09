6df1e0d7804be6596f32d93d7a51aa3b864afcec
Passapol <phasphlsuthahthrrm976@gmail.com>
2024-05-09 23:39:38 +0700

    update 9-5-2567

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # sendGmailNodeJS
- 
+<h1>code nodejs express sendGmail.</h1>
+<hr>
+<h1>set ting .env</h1>
+<p>PORT_VERSER=***</p>
+<p>USER_GMAIL=***</p>
+<p>PASS_GMAIL=***</p>
+<hr>
+<h1>How to get USER_GMAIL and PASS_GMAIL</h1>
+<li>use you Gmail in .env file.</li>
+<li><a target="_blank" href="https://myaccount.google.com/u/2/apppasswords">https://myaccount.google.com/u/2/apppasswords</a> create app google will give "PASS_USER" for yor,And use PASS in .env fiel.</li>
---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = express();
+const nodemailer = require('nodemailer');
+const dotenv = require('dotenv');
+dotenv.config();
+/*
+    seting config .env
+    {
+        PORT_VERSER=***
+        USER_GMAIL=***
+        PASS_GMAIL=***
+    }
+*/
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.USER_GMAIL,
+        pass: process.env.PASS_GMAIL
+    }
+})
+/*
+    POST request send email and toKen
+*/
+app.get('/sendgmail', (req, res) => {
+    try{
+        const gmail = "phasphlsuthathrrm976@gmail.com";
+        const status = sendResetPasswordEmail(gmail);
+        res.json(status);
+    }catch{
+        res.json({status:500,message:"error"});
+    }
+})
+/* 
+    function send reset password to email
+*/
+const sendResetPasswordEmail = (to) => {
+    const mailOptions = {
+      from: 'test@gmail.com',
+      to,
+      subject: 'hello',
+      html: `<p>test nodejs send Gmail.</p>`,
+    };
+    try{
+        transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+            console.error('Error sending email:', error);
+          } else {
+            console.log('Email sent:', info.response);
+          }
+        });
+        return {status:200,message:"ok"}
+    }catch{
+        return {status:501,message:"error"}
+    }
+};
+
+
+
+app.listen(process.env.PORT_VERSER,()=>{console.log("http://localhost:"+process.env.PORT_VERSER)})
